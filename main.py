33512d9931ebedc8ae37ee8a66cbf8b574b07d15
arij <m'hiri.arij@esprit.tn>
2024-09-02 19:47:15 +0100

    improved OCR quality

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
-from util import detect_id, segment_id
-from PIL import Image
+from util import IDDetector, IDSegmenter
 import pytesseract
-
+import re
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import defaultdict
+import cv2
 
 app = FastAPI()
+# Usage
+detector = IDDetector('classification.pt')
+segmenter = IDSegmenter('segmentation.pt')
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Define a dictionary to map common OCR errors to their corrections
+# correction_dict = defaultdict(str)
+# correction_dict['ا'] = 'أ'
+# correction_dict['آ'] = 'أ'
+# correction_dict['إ'] = 'إ'
+# correction_dict['ؤ'] = 'ؤ'
+# correction_dict['ئ'] = 'ئ'
+
+# Define a function to preprocess the image
+def preprocess_image(image_path):
+    # You can add image preprocessing steps here, such as binarization, deskewing, etc.
+    return image_path
+
+# Define a function to postprocess the OCR output
+def post_process_text(text):
+    # Remove non-Arabic characters
+    cleaned_text = re.sub(r'[^أ-ي\s]', '', text)
+    
+    # Remove diacritics
+    cleaned_text = re.sub(r'[\u0610-\u061A\u064B-\u065F]', '', cleaned_text)
+    
+    # Remove extra spaces
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+    
+    # Correct common OCR errors
+    # for key, value in correction_dict.items():
+    #     cleaned_text = cleaned_text.replace(key, value)
+    
+    # Tokenize the text
+    tokens = word_tokenize(cleaned_text)
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('arabic'))
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # Join the tokens back into a string
+    cleaned_text = ' '.join(tokens)
+    
+    return cleaned_text
 
 @app.get("/")
 def read_root():
@@ -21,19 +70,24 @@ async def predict_image(file: UploadFile = File(...)):
         with open(temp_image_path, "wb") as f:
             f.write(image_data)
 
+        # Preprocess the image
+        # preprocessed_image_path = preprocess_image(temp_image_path)
+        img_cv = cv2.imread(temp_image_path)
+
         # Now use the path for processing
-        img_class = detect_id(temp_image_path)
+        img_class = detector.detect(temp_image_path)
         if img_class == "id":
-            seg_img = segment_id(temp_image_path)
+            seg_img = segmenter.segment(temp_image_path)
             if not seg_img:
                 raise HTTPException(status_code=400, detail="No regions detected in the image.")
             results = []
+            custom_config = r'--oem 1 --psm 6 -c tessedit_char_blacklist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
             for i, seg in enumerate(seg_img):
-                # Ensure that each segment is a PIL image
-                if not isinstance(seg, Image.Image):
-                    raise HTTPException(status_code=400, detail=f"Segment {i+1} is not a valid image object.")
-                text = pytesseract.image_to_string(seg, lang='ara')
-                results.append({"segment": i+1, "text": text})
+                
+                text = pytesseract.image_to_string(seg, lang='ara+ara_number',config= custom_config )
+                cleaned_text = post_process_text(text)
+                results.append(cleaned_text)
             return {"id informations": results}
         else:
             return {"detail": "This image is not an ID"}
--- util.py
+++ util.py
@@ -3,40 +3,72 @@ import numpy as np
 from PIL import Image
 import cv2
 
+class IDDetector:
+    def __init__(self, model_path):
+        self.detector = YOLO(model_path)
 
-id_detector=YOLO('best-cls.pt')
-id_segmentation=YOLO('best-seg.pt')
-
-def detect_id(image: Image.Image):
-    results = id_detector(image)
-    names_dict = results[0].names
-    probs = results[0].probs.data.tolist() 
-    detected_class = names_dict[np.argmax(probs)]  
-    
-    return detected_class
-def segment_id(image: Image.Image):
-    
-    results = id_segmentation(image)
-    boxes = results[0].boxes.xyxy.cpu().numpy()
-    confidences = results[0].boxes.conf.cpu().numpy()
-    class_ids = results[0].boxes.cls.cpu().numpy()
-
-    highest_confidence_boxes = {}
-    for box, confidence, class_id in zip(boxes, confidences, class_ids):
-   
-        if class_id not in highest_confidence_boxes or confidence > highest_confidence_boxes[class_id]['confidence']:
-            highest_confidence_boxes[class_id] = {
-                'box': box,
-                'confidence': confidence
-        }
-
-
-    segmented_images = []
-    for entry in highest_confidence_boxes.values():
-        box = entry['box']
-        x1, y1, x2, y2 = box.astype(int)
-        segmented_image = Image.fromarray(cv2.cvtColor(cv2.imread(image)[y1:y2, x1:x2], cv2.COLOR_BGR2RGB))
-        segmented_images.append(segmented_image)
+    def detect(self, image: Image.Image):
+        results = self.detector(image)
+        names_dict = results[0].names
+        probs = results[0].probs.data.tolist() 
+        detected_class = names_dict[np.argmax(probs)]  
         
+        return detected_class
 
-    return segmented_images
+class IDSegmenter:
+    def __init__(self, model_path):
+        self.segmenter = YOLO(model_path)
+
+    def segment(self, image: Image.Image):
+        results = self.segmenter(image)
+        boxes = results[0].boxes.xyxy.cpu().numpy()
+        confidences = results[0].boxes.conf.cpu().numpy()
+        class_ids = results[0].boxes.cls.cpu().numpy()
+
+        highest_confidence_boxes = {}
+        for box, confidence, class_id in zip(boxes, confidences, class_ids):
+            if class_id not in highest_confidence_boxes or confidence > highest_confidence_boxes[class_id]['confidence']:
+                highest_confidence_boxes[class_id] = {
+                    'box': box,
+                    'confidence': confidence
+                }
+
+        segmented_images = []
+        for entry in highest_confidence_boxes.values():
+            box = entry['box']
+            x1, y1, x2, y2 = box.astype(int)
+            segmented_image = Image.fromarray(cv2.cvtColor(cv2.imread(image)[y1:y2, x1:x2], cv2.COLOR_BGR2RGB))
+            segmented_images.append(segmented_image)
+
+        return segmented_images
+
+class PreprocessImage:
+    def __init__(self, image_path: str):
+        self.image_path = image_path
+        self.image = self._load_image()
+
+    def _load_image(self) -> np.ndarray:
+        """Load the image from the file path."""
+        return cv2.imread(self.image_path)
+
+    def to_grayscale(self) -> np.ndarray:
+        """Convert the image to grayscale."""
+        return cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+    def apply_gaussian_blur(self, image: np.ndarray) -> np.ndarray:
+        """Apply Gaussian Blur to the image."""
+        return cv2.GaussianBlur(image, (5, 5), 0)
+
+    def apply_thresholding(self, image: np.ndarray) -> np.ndarray:
+        """Apply thresholding to convert the image to binary."""
+        _, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        return thresh
+
+    def preprocess(self) -> np.ndarray:
+        """Apply the full preprocessing pipeline."""
+        gray_image = self.to_grayscale()
+        blurred_image = self.apply_gaussian_blur(gray_image)
+        preprocessed_image = self.apply_thresholding(blurred_image)
+        return preprocessed_image
+
+
